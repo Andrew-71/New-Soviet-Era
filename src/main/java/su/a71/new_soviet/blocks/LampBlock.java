e3eeccfd60520da65b169a567dc27693491118fc
Andrew-71 <andrey.nikitin.vladimirovich@gmail.com>
2023-07-17 21:48:00 +0300

    Fix lamps

--- src/main/java/su/a71/new_soviet/blocks/LampBlock.java
+++ src/main/java/su/a71/new_soviet/blocks/LampBlock.java
@@ -83,10 +83,30 @@ public class LampBlock extends Block implements Waterloggable {
         return false;
     }
 
+    public static VoxelShape getHangingShape(){
+        VoxelShape shape = VoxelShapes.empty();
+        shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.34375, -0.221875, 0.34375, 0.65625, 0.090625, 0.65625));
+        shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.125, 0.0625, 0.125, 0.875, 0.4375, 0.875));
+        shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.5, 0.5, 0.125, 0.5, 0.875, 0.875));
+        shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.09375, 0.4375, 0.09375, 0.90625, 0.5, 0.15625));
+        shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.4375, 0.875, 0.4375, 0.5625, 0.90625, 0.5625));
+        shape.simplify();
+        return shape;
+    }
+
+    public static VoxelShape getStandingShape(){
+        VoxelShape shape = VoxelShapes.empty();
+        shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.1875, 0.4375, 0.1875, 0.8125, 1.0625, 0.8125));
+        shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.375, 0, 0.375, 0.625, 0.25, 0.625));
+        shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.4375, 0.25, 0.4375, 0.5625, 0.875, 0.5625));
+        shape.simplify();
+        return shape;
+    }
+
     static {
         HANGING = Properties.HANGING;
         WATERLOGGED = Properties.WATERLOGGED;
-        STANDING_SHAPE = VoxelShapes.union(Block.createCuboidShape(5.0, 0.0, 5.0, 11.0, 7.0, 11.0), Block.createCuboidShape(6.0, 7.0, 6.0, 10.0, 9.0, 10.0));
-        HANGING_SHAPE = VoxelShapes.union(Block.createCuboidShape(5.0, 1.0, 5.0, 11.0, 8.0, 11.0), Block.createCuboidShape(6.0, 8.0, 6.0, 10.0, 10.0, 10.0));
+        STANDING_SHAPE = getStandingShape();
+        HANGING_SHAPE = getHangingShape();
     }
 }
